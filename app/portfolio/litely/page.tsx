e13ced9486720fe030c76e8022c3e730fae7c71f
Mansi Kumawat <mansikumawat@Mansis-iMac.local>
2025-08-08 03:05:39 +0530

    updated portfolio

--- app/portfolio/litely/page.tsx
+++ app/portfolio/litely/page.tsx
@@ -101,7 +101,7 @@ const PortfolioDetailPage = () => {
                             ))}
                         </div>
 
-                        <Link href="https://play.google.com/store/apps/details?id=com.eskritor.app" className="font-bold text-[15px] flex gap-2 items-center mt-4 hover:bg-[#F2F2F2] p-2 w-fit rounded-md leading-[16px] hover:text-[#141DEA] group transition-all duration-500 ease-in-out">
+                        <Link href="https://play.google.com/store/apps/details?id=lose.weight.together" className="font-bold text-[15px] flex gap-2 items-center mt-4 hover:bg-[#F2F2F2] p-2 w-fit rounded-md leading-[16px] hover:text-[#141DEA] group transition-all duration-500 ease-in-out">
                             Android App <ArrowRight className="w-5 h-5 transition-transform duration-200 ease-in-out group-hover:translate-x-1 -translate-x-1" />
                         </Link>
                     </div>
